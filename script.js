05800e9b2908216db8502caebc335efe6ba79dbc
praisejah uzoma <praisejah.c.uzoma@gmail.com>
2023-09-13 16:14:36 +0100

    feat: :sparkles: Initialize Firebase App and Add Data to Shopping List
    add Firebase initialization to the app and configured it to connect to the database.

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://my-cart-57079-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 
 addButtonEl.addEventListener("click", function() {
     let data = inputFieldEl.value
+
+    push( shoppingListInDB, data)
     console.log(data)
 })
 
