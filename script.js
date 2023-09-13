03e2f9c17b20a39009b299b8df536028c1c2b273
praisejah uzoma <praisejah.c.uzoma@gmail.com>
2023-09-13 16:17:59 +0100

    docs: Add comments to Firebase and Shopping List JavaScript code

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+// Import Firebase SDK modules for initialization and database operations
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
+// Firebase configuration settings for your app
 const appSettings = {
     databaseURL: "https://my-cart-57079-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
-const app = initializeApp(appSettings)
-const database = getDatabase(app)
-const shoppingListInDB = ref(database, "shoppingList")
+// Initialize the Firebase app with the provided settings
+const app = initializeApp(appSettings);
 
-const inputFieldEl = document.getElementById("input-field")
-const addButtonEl = document.getElementById("add-button")
+// Get a reference to the Firebase Realtime Database
+const database = getDatabase(app);
 
+// Define a reference to the "shoppingList" location in the database
+const shoppingListInDB = ref(database, "shoppingList");
+
+// Get references to HTML elements
+const inputFieldEl = document.getElementById("input-field");
+const addButtonEl = document.getElementById("add-button");
+
+// Add a click event listener to the "Add" button
 addButtonEl.addEventListener("click", function() {
-    let data = inputFieldEl.value
+    // Get the data (item) from the input field
+    let data = inputFieldEl.value;
 
-    push( shoppingListInDB, data)
-    console.log(data)
-})
+    // Push the data (item) to the "shoppingList" in the database
+    push(shoppingListInDB, data);
 
+    // Log the data to the console (for debugging or confirmation)
+    console.log(data);
+});
